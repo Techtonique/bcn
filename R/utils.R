e2dc48c21cc1b4f37edf8afefe59681c7a4bfb85
thierrymoudiki <thierry.moudiki@gmail.com>
2022-08-11 22:03:08 +0200

    one hot encode when labels start at 0

--- R/utils.R
+++ R/utils.R
@@ -67,9 +67,10 @@ my_sd <- compiler::cmpfun(my_sd)
 # bcn::one_hot_encode(y, 3)
 one_hot_encode <- function(y, n_classes)
 {
+  # y must me `numeric`
   n_obs <- length(y)
   res <- matrix(0, nrow=n_obs, ncol=n_classes)
-  if (min(as.numeric(y)) == 0) # input index starting at 0 (index in R start at 1)
+  if (min(y) == 0) # input index starting at 0 (index in R start at 1)
   {
     y_ <- y + 1L
     for (i in 1:n_obs){
